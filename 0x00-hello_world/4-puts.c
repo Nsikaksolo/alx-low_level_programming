ffdb36c5fb47edd6681f3aec01c280d6105e682d
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-08 15:33:52 -0700

    my C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return: Always 0 (Successful)
+ * */
+int main(void)
+{
+	puts("\"programmingis like building a multilingual puzzle");
+	return (0);
+
+}
