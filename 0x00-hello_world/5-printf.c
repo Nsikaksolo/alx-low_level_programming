0bc3d52bc0f62d789c10ab973b48268820be1b0a
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-11 16:09:05 -0700

    my c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - print the string in the put function
- * Description: using the main function
+ *
  * this program prints with proper grammar, but the outcome is a piece of art
- * return: 0
+ * return: Always 0 (Succss)
  */
 int main(void)
 {
