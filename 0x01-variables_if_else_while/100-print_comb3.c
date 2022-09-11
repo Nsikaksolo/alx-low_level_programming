7bb9f9ea2a0c58b7f2ac850a93db3b55d8937f34
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-11 14:29:20 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#iinclude <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main -Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int i, j;
