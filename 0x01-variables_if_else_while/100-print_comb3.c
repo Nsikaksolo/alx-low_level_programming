413ec6943fa07a540306292cc5d4d9fb6dbcb62f
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-11 14:56:20 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,21 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int i, j;
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = i + 1; j <= 10; j++)
 		{
-			if (j > i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 8)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 return (0);
 }
