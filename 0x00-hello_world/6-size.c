17dee99dd8effa48e74db7fa1dbef458d72374aa
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-11 18:05:34 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 #include <math.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,11 +11,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-
-	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
-	rintf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	rintf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
