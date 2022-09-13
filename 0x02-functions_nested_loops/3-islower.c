f78078b4c3f8e3fdf9a8987081fe721336cef7ee
Nsikaksolo <solomonnsikak748@gmail.com>
2022-09-13 16:22:54 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - function to check for lowercas
+ * @c: is the int that will be use for the aurgument of the function
+ * Reeturn: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122);
+	return (0);
+}
